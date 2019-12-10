a7171ccf45a15b667ea61445462c928bea806233
kiam <kiam125@gmail.com>
2019-12-11 02:37:15 +0800

    Feature:view use in function

--- app/src/main/java/com/example/kr_linechatappication/Activitity/MainActivity.java
+++ app/src/main/java/com/example/kr_linechatappication/Activitity/MainActivity.java
@@ -22,11 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtReply = (EditText) findViewById(R.id.edt_reply) ;
-        chatRecyclerView = (RecyclerView) findViewById(R.id.chatRecyclerView);
-        chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        chatAdapter = new ChatAdapter(this);
-        chatRecyclerView.setAdapter(chatAdapter);
+        initView();
+        setRecyclerView();
 //
         chatAdapter.addItem(new ChatData("Takuma Lee","你好啊，Kr Lee 我是你的導師","",2));
         chatAdapter.addItem(new ChatData("Kr Lee","你好， 我叫Kr Lee，很開心認識你，有幸在你的領導之下，可以變得更強","",1));
@@ -35,5 +32,16 @@ public class MainActivity extends AppCompatActivity {
     public void onSendClick(View view) {
         chatAdapter.addItem(new ChatData("Kr Lee",edtReply.getText().toString(),"",1));
         edtReply.setText("");
+    }
+
+    private void initView() {
+        edtReply = (EditText) findViewById(R.id.edt_reply) ;
+        chatRecyclerView = (RecyclerView) findViewById(R.id.chatRecyclerView);
+    }
+
+    private void setRecyclerView() {
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        chatAdapter = new ChatAdapter(this);
+        chatRecyclerView.setAdapter(chatAdapter);
     }
 }
