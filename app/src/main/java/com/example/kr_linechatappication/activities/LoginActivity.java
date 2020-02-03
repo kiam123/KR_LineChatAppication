e7dc7d7611228401e80279ce28b9ab4bc1045ba7
kr-lee <kiam125@gmail.com>
2020-02-04 03:01:35 +0800

    Feature: login page will check account is exist

--- app/src/main/java/com/example/kr_linechatappication/activities/LoginActivity.java
+++ app/src/main/java/com/example/kr_linechatappication/activities/LoginActivity.java
@@ -1,27 +1,72 @@
 package com.example.kr_linechatappication.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kr_linechatappication.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ThrowOnExtraProperties;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TextView txtAccount;
+    TextView txtPassword;
+    String account, password;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        initView();
+        setToolbar();
+    }
 
+    public void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        txtAccount = (TextView) findViewById(R.id.txtAccount);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
+    }
+
+    private void setToolbar() {
+        toolbar.setTitle("KRLineChatAppication");
     }
 
     public void onLoginClick(View view) {
-        Intent intent = new Intent();
-        intent.setClass(this, MainActivity.class);
-        startActivity(intent);
+        account = txtAccount.getText().toString();
+        password = txtPassword.getText().toString();
+
+        myRef.child("users").child(account).child("password").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue().toString().equals(password)) {
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "賬號或密碼錯誤", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+
     }
 
     public void onRegisterClick(View view) {
--- app/src/main/java/com/example/kr_linechatappication/activities/RegisterActivity.java
+++ app/src/main/java/com/example/kr_linechatappication/activities/RegisterActivity.java
@@ -1,16 +1,29 @@
 package com.example.kr_linechatappication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.example.kr_linechatappication.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initView();
+        setToolbar();
+    }
+
+    public void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    private void setToolbar() {
+        toolbar.setTitle("註冊");
     }
 }
